b681a7ec6c5b3116916b428b5438ecac5f537491
Eknoor Oberoi <38902676+eknooroberoi@users.noreply.github.com>
2020-06-17 04:03:31 +0530

    React conditional rendering with ternary and AND operator

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,12 @@
 import React from "react";
-import Login from "./Login";
+import Form from "./Form";
 
-var isLoggedIn = false;
-
-const currentTime = new Date(2019, 11, 1, 9).getHours();
-console.log(currentTime);
-
-//we can return only expression, not statement or condition, here in order to return condition use ternary operator, it makes it into expression
+var userIsRegistered = false;
 
 function App() {
   return (
     <div className="container">
-      {/*Ternary Operator*/}
-      {isLoggedIn ? <h1>Hello</h1> : <Login />}
-      {/*AND Operator*/}
-      {currentTime > 12 && <h1>Why are you still working?</h1>}
+      <Form />
     </div>
   );
 }
